eb5ee96889ecd2b891f6290fd5c9f35a84efc993
Jeff Brunsek <jbrunsek@opentext.com>
2016-10-27 14:06:45 -0400

    added user lookup for possibly OOO

--- lib/lita/handlers/outofoffice.rb
+++ lib/lita/handlers/outofoffice.rb
@@ -4,7 +4,8 @@ module Lita
   module Handlers
     class Outofoffice < Handler
 
-      route(/^ooo\s(.+)/i, :setOOO, command: true, help: { "ooo" => "Set out of office response"})
+      route(/^ooo\s(.+)/i, :setOOO, command: true, help: { "ooo" => "Set days you will be out of office, ie 'ooo from today until Friday'"})
+      route(/@/i, :isOOO, command: false)
 
       def fromDate(str)
       	str[/#{Regexp.escape("from ")}(.*?)#{Regexp.escape(" until")}/m, 1]
@@ -26,6 +27,15 @@ module Lita
 	date.strftime("%Y-%m-%d")
       end
 
+      def checkOOO(out, back)
+	now = DateTime.now.to_date
+        if now >= out and now <= back
+          (back - out).to_i
+        else
+          0
+        end
+      end
+
       def setOOO(response)
 	begin
 	  from=dateCheck(fromDate(response.message.body))
@@ -36,12 +46,33 @@ module Lita
 	  end	
 
 	  out = { out: from, in: to }
-          redis.set(response.user.name, out)
+          redis.set(response.user.mention_name, out)
 
           response.reply("Okay #{response.user.name}, I've got you marked as Out of Office from #{from} until #{to}")
 	rescue ArgumentError
           response.reply("Something about your dates look funny to me. Fix it and try again")
         end
+      end
+
+      def isOOO(response)
+	response.message.body.split(" ").each { |w|
+	  if w.include? "@" and w.include? "jbrunsek"
+	    data = eval(redis.get(w.split('@')[1]))
+            #response.reply(data[:out]}
+	    if !data[:out].nil?
+	      duration = checkOOO(DateTime.parse(data[:out]), DateTime.parse(data[:in]))
+	      if duration > 7
+                response.reply("#{w} is currently out of office until...#{DateTime.parse(data[:in]).strftime("%b %d")}?! Are we still paying this human?")
+	      elsif duration > 0
+                if response.message.body.include? "?"
+                  response.reply("#{w} is currently out of office. They needed a break because you ask them too many questions.")
+                else
+		  response.reply("It looks like #{w} is currently out of office and won't be back until #{data[:in]}")
+		end
+	      end
+            end
+	  end
+	}
       end
 
       Lita.register_handler(self)
